a3d8d65e566781c42c6394321c527d3f8d849e2d
EACRYO <eacryo@gmail.com>
2024-12-24 22:00:40 +0800

    init project

---
+++ src/test/java/com/eacryo/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eacryo.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
